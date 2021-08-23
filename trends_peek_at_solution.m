38829ee887b24f45bdd07cf86f43d37308657695
Brice Green <bcg@msm.mit.edu>
2021-08-23 19:24:44 -0400

    update w/ timestamp logic

--- trends_peek_at_solution.m
+++ trends_peek_at_solution.m
@@ -1,4 +1,4 @@
-
+timestamp = '23-Aug-2021 11_14_09';
 function_vals = [];
 sols = [];
 
@@ -13,14 +13,23 @@ for i = 1:1000
     
        close all     
        % so we can hack it by running publish code
-        outdir = ['./model-output/model-run-number',num2str(i)];
+        outdir = ['./model-output_',timestamp, '/model-run-number',num2str(i)];
         addpath(outdir);
 
         theseoptions = struct('showCode', false, 'format','pdf','outputDir',outdir);
-        publish([outdir, '/publishcode.m'], theseoptions)
         
-        function_vals = [function_vals; i, ans];
+        fid = fopen([outdir, '/publishcode.m']);
+        % we want the second line, this gets it
+        line = fgetl(fid);
+        line = fgetl(fid);
+        fclose(fid)
+        
+        eval(line);
+        loss = lrtmodel(this_solution, 0, 0, 80, 'parse_model_params_v2');
+        
+        function_vals = [function_vals; i, loss];
         sols = [sols; this_solution];
+        rmpath(outdir);
    catch    
    end 
 end    
@@ -61,9 +70,14 @@ for j = 1:5
     trunc_fvals(best_index_vec,:) = []; % delete best one;
     trunc_sols(best_index_vec,:) = []; % delete best one;
     
+    publishpath = ['./model-output_', timestamp, '/model-run-number',num2str(best_i)];
+    
+    addpath(publishpath)
+    publish([publishpath, '/publishcode.m'], theseoptions)
+    rmpath(publishpath)
+    
     % now, copy over the output file
-     inputfile = ['./model-output/model-run-number',...
-                   num2str(best_i),'/publishcode.pdf'];
+     inputfile = [publishpath,'/publishcode.pdf'];
 
      outputfile = [outdir,'/publishcode_best',num2str(j),'.pdf'];
      copyfile(inputfile,outputfile);  
