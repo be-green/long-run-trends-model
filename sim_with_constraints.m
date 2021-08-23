fa872317528c62b2ee542b8cc95218bb963792f1
ldwschmidt <ldwschmidt@uchicago.edu>
2021-08-22 22:39:58 -0400

    forgot to stage this in last commit. Adding it now

--- sim_with_constraints.m
+++ sim_with_constraints.m
@@ -1,4 +1,7 @@
-function startvalues = sim_with_constraints(N, upper, lower, Aineq, bineq)
+function startvalues = sim_with_constraints(N, upper, lower, Aineq, bineq, parse_fcn_name)
+
+if strcmp(parse_fcn_name,'parse_model_params_v1')
+
    which_unconstrained = sum(Aineq) == 0; 
    
    % weird bug means I can't do this directl
@@ -14,7 +17,7 @@ function startvalues = sim_with_constraints(N, upper, lower, Aineq, bineq)
    alpha_lower = max(lower(2), sims(:,3) / 0.1);
    alpha_upper = upper(2);
    
-   sims(:, 2) = unifrnd(alpha_lower, alpha_upper, [N, 1]);
+   sims(:, 2) = unifrnd(alpha_lower, min(max(alpha_upper,alpha_lower+1e-4),1), [N, 1]);
    
    phi_upper = (0.9523 * sims(:,3) + bineq(1)) / 0.0477;
    phi_lower = (0.9 * sims(:,3) + bineq(2)) / 0.1;
@@ -22,4 +25,8 @@ function startvalues = sim_with_constraints(N, upper, lower, Aineq, bineq)
    sims(:, 1) = unifrnd(phi_lower, phi_upper, [N, 1]);
    
    startvalues = sims;
+else
+    error('Parsing function not yet adapted.')
+end  
+
 end
