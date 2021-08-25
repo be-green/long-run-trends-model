de9a998cd9ffbe7ab83bfe62018357a958b90983
Brice Green <brice.c.green@gmail.com>
2021-08-24 18:22:30 -0700

    up cpu mem requirement

--- run-script.sh
+++ run-script.sh
@@ -2,7 +2,7 @@
 #SBATCH -n 24 #Request 4 tasks (cores)
 #SBATCH -t 0-12:30 #Request runtime of 30 minutes
 #SBATCH -p sched_mit_sloan_batch #Run on sched_engaging_default partition
-#SBATCH --mem-per-cpu=2000 #Request 4G of memory per CPU
+#SBATCH --mem-per-cpu=8GB #Request 4G of memory per CPU
 #SBATCH -o output_%j.txt #redirect output to output_JOBID.txt
 #SBATCH -e error_%j.txt #redirect errors to error_JOBID.txt
 #SBATCH --mail-type=BEGIN,END #Mail when job starts and ends
