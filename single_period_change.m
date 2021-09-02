8e552c6672fd12ecbbb8912e4370b981151e03f8
Brice Green <bcg@msm.mit.edu>
2021-09-02 16:20:16 -0400

    add assertion to force sum-to-1 for probabilities during transitions

--- single_period_change.m
+++ single_period_change.m
@@ -1,5 +1,8 @@
 function end_grid = single_period_change(grid, s0_trans, s1_trans,...
     s0_intercept, s1_intercept, omega)
-    end_grid = (1 - omega) * (s0_trans * grid + s0_intercept) ...
+    newgrid = (1 - omega) * (s0_trans * grid + s0_intercept) ...
         + omega * (s1_trans * grid + s1_intercept);
+    % needs to sum to 1
+    assert(sum(newgrid) - 1 < 1e-10, 'Probability density on grid failed to sum to 1 following transition.' );
+    end_grid = newgrid;
 end
